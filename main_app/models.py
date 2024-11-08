dad1b5ad7f3ca37911cae63470d65c283139c9f7
ScottKinnear <gr8whtninjassk@gmail.com>
2024-11-07 17:53:19 -0800

    Admin user and cats addeed to db

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,7 @@ class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
-    age = models.IntegerField
+    age = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
