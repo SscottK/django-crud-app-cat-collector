ef43e18592970509cb710ece319b4e05246757d7
ScottKinnear <gr8whtninjassk@gmail.com>
2024-11-12 16:32:20 -0800

    Full CRUD for Cat model

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
 
 # Create your views here.
 
@@ -14,3 +15,24 @@ def about(request):
 def cat_index(request):
     cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+#single cat view
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+
+
+#Cat creat class
+class CatCreate(CreateView):
+    model = Cat
+    fields = "__all__"
+    # success_url = '/cats/'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
